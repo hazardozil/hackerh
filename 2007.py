36c2c79889ea7d0675ca7698e3a64ccded7d1b84
waelhacer <139044485+waelhacer@users.noreply.github.com>
2023-08-24 20:15:02 +0200

    Update 2007.py

--- 2007.py
+++ 2007.py
@@ -103,7 +103,7 @@ t=(mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
 
 hours = (now.hour)
 x = datetime.datetime.now()
-g= datetime.datetime(2023, 7, 25, 23, 00 ,9)
+g= datetime.datetime(2023, 8, 25, 23, 00 ,9)
 
 
 if (x.strftime("%x"))>(g.strftime("%x")):
