f72e0937e399e89c36ffb2cecec40fa3ccd9fb00
hazardozil <139044485+hazardozil@users.noreply.github.com>
2023-10-30 16:15:57 +0200

    Update قديم نار.py

--- قديم نار.py
+++ قديم نار.py
@@ -74,7 +74,7 @@ t=(mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
 
 hours = (now.hour)
 x = datetime.datetime.now()
-g= datetime.datetime(2023, 9, 18, 23, 00 ,9)
+g= datetime.datetime(2023, 11, 18, 23, 00 ,9)
 
 
 if (x.strftime("%x"))>(g.strftime("%x")):
