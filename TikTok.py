07544cad35a12a46fa0c33ad3c52599086d5f68b
hazardozil <139044485+hazardozil@users.noreply.github.com>
2023-11-24 07:44:35 +0200

    Update TikTok.py

--- TikTok.py
+++ TikTok.py
@@ -54,7 +54,7 @@ t=(mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
 
 hours = (now.hour)
 x = datetime.datetime.now()
-g= datetime.datetime(2023, 8, 28, 23, 00 ,9)
+g= datetime.datetime(2023, 11, 28, 23, 00 ,9)
 
 
 if (x.strftime("%x"))>(g.strftime("%x")):
