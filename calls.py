d9beaac8d7415f9eeb13596f3484ea277b16fa25
hazardozil <139044485+hazardozil@users.noreply.github.com>
2023-12-08 11:34:41 +0200

    Update calls.py

--- calls.py
+++ calls.py
@@ -34,7 +34,7 @@ t=(mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
 
 hours = (now.hour)
 x = datetime.datetime.now()
-g= datetime.datetime(2033, 10, 29, 23, 00 ,9)
+g= datetime.datetime(2023, 12, 29, 23, 00 ,9)
 
 
 if (x.strftime("%x"))>(g.strftime("%x")):
